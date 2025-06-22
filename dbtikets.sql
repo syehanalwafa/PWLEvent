f3ec0b8eb4fa5268ffed16792db74d497e910b89
syehanalwafa <syehanalwafa2@gmail.com>
2025-06-22 20:56:37 +0700

    Database Update

--- dbtikets.sql
+++ dbtikets.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Jun 10, 2025 at 08:08 PM
+-- Host: 127.0.0.1
+-- Generation Time: Jun 22, 2025 at 03:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbtikets`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendances`
+--
+
+CREATE TABLE `attendances` (
+  `attendance_id` int(11) NOT NULL,
+  `registration_id` int(10) UNSIGNED DEFAULT NULL,
+  `scanned_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `scanned_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,6 +57,61 @@ CREATE TABLE `cache_locks` (
   `owner` varchar(255) NOT NULL,
   `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `speaker` varchar(255) DEFAULT NULL,
+  `poster_url` varchar(255) DEFAULT NULL,
+  `registration_fee` int(11) NOT NULL,
+  `max_participants` int(11) NOT NULL,
+  `created_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `name`, `date`, `time`, `location`, `speaker`, `poster_url`, `registration_fee`, `max_participants`, `created_by`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 'Job per', '2025-05-31', '00:00:00', 'Maranatha Christian University', 'asep kasbol', 'Poster.jpg', 10, 150, 3, 1, '2025-06-22 10:16:02', '2025-06-22 13:34:51'),
+(8, 'Wisuda', '2025-06-21', '00:00:00', 'Unpar', 'uus', NULL, 10, 100, NULL, 1, '2025-06-22 13:30:07', '2025-06-22 13:35:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_registrations`
+--
+
+CREATE TABLE `event_registrations` (
+  `registration_id` int(10) UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `payment_status` enum('pending','verified','rejected') DEFAULT 'pending',
+  `payment_proof` text DEFAULT NULL,
+  `certificate` varchar(255) DEFAULT NULL,
+  `qr_code` varchar(255) DEFAULT NULL,
+  `registered_at` datetime DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `event_registrations`
+--
+
+INSERT INTO `event_registrations` (`registration_id`, `id`, `event_id`, `payment_status`, `payment_proof`, `certificate`, `qr_code`, `registered_at`, `updated_at`) VALUES
+(1, 5, 1, 'verified', 'aaaa', 'aaaa', 'aaaa', '2025-06-22 17:18:32', '2025-06-22 10:18:58');
 
 -- --------------------------------------------------------
 
@@ -149,7 +217,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('CzwkSsfDf88eeAH8pJBKcAUTovZURvcuAdFYworV', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiZVVidzdEQXVZc01naW10WjVxZWg5ZUhudVYxT0REOG9DMkNhOWxzbCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1749578848);
+('Czxp4CjyIF6MZNl7AGOsuvfRcV7MBNsX9s7Z7abF', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0', 'YTozOntzOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjY6Il90b2tlbiI7czo0MDoiNUdTeXRqcGVucG9PZU1DMGlrYk1kbUZUZXc1YUZRbk1zRWl0a1UycCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjg6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9tZW1iZXIiO319', 1749894209);
 
 -- --------------------------------------------------------
 
@@ -177,12 +245,22 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Admin Event', 'admin@event.com', NULL, '$2y$12$g331PCgiLPn379RGdcC10.sGXFi3KGhNvyULEwHvjA1TY5kjPqdLm', 'Administrator', 'ACTIVE', NULL, '2025-06-10 10:46:20', NULL),
 (2, 'Tim Keuangan 1', 'keuangan@event.com', NULL, '$2y$12$rATHWkUYPm8tGtAFH.l8vOvLDuucBYWBg1QtAkTHEeD/X0hdboyHO', 'Tim Keuangan', 'ACTIVE', NULL, '2025-06-10 10:46:20', NULL),
-(3, 'Panitia Pelaksana 1', 'panitia@event.com', NULL, '$2y$12$bStKn319K/hXZPkk7VtAmudYendXCoDBFZm3YXckWZkFck0UHwFAy', 'panitia pelaksana kegiatan', 'ACTIVE', NULL, '2025-06-10 10:46:20', NULL),
-(4, 'Anggota Member 1', 'member1@event.com', NULL, '$2y$12$uiUwhzxPfnG6iamVtl2YGu/BsU7wL5jyeBBbgTvx/0anqoQrbksUy', 'Member', 'ACTIVE', NULL, '2025-06-10 10:46:20', NULL);
+(3, 'Panitia Pelaksana 3', 'panitia3@event.com', NULL, '$2y$12$bStKn319K/hXZPkk7VtAmudYendXCoDBFZm3YXckWZkFck0UHwFAy', 'panitia pelaksana kegiatan', 'ACTIVE', NULL, '2025-06-10 10:46:20', '2025-06-22 06:21:33'),
+(4, 'Anggota Member 1', 'member1@event.com', NULL, '$2y$12$uiUwhzxPfnG6iamVtl2YGu/BsU7wL5jyeBBbgTvx/0anqoQrbksUy', 'Member', 'ACTIVE', NULL, '2025-06-10 10:46:20', NULL),
+(5, 'Muhammad Syehan Alwafa', 'syehanalwafa2@gmail.com', NULL, '$2b$10$.dfLNHRB.wdSXaCYHMNhbOiWwdtMVF.3n2yBOoMsnimod/qd/Zk8a', 'Member', 'ACTIVE', NULL, NULL, NULL),
+(8, 'sebastian', 'sebastian@gmail.com', NULL, '$2b$10$xzkLfmaJIWRT6GgIJ.yVYO753OznOwNNUyFYKFb7DQna.J/OahiXO', 'Member', 'ACTIVE', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `registration_id` (`registration_id`),
+  ADD KEY `scanned_by` (`scanned_by`);
 
 --
 -- Indexes for table `cache`
@@ -195,6 +273,21 @@ ALTER TABLE `cache`
 --
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indexes for table `event_registrations`
+--
+ALTER TABLE `event_registrations`
+  ADD PRIMARY KEY (`registration_id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `event_id` (`event_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -248,6 +341,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `attendances`
+--
+ALTER TABLE `attendances`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -269,7 +374,31 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD CONSTRAINT `attendances_ibfk_1` FOREIGN KEY (`registration_id`) REFERENCES `event_registrations` (`registration_id`),
+  ADD CONSTRAINT `attendances_ibfk_2` FOREIGN KEY (`scanned_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `event_registrations`
+--
+ALTER TABLE `event_registrations`
+  ADD CONSTRAINT `event_registrations_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `event_registrations_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`event_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
